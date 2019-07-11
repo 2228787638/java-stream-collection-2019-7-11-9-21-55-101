cf7da19f16929422fd21f214547f403e0a0d9c7b
2228787638 <2228787638@qq.com>
2019-07-12 01:45:07 +0800

    feat: implement all

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,23 +2,53 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        if(left<right) {
+            for (int i = left; i <= right; i++) {
+                list.add(i);
+            }
+            return list;
+        }else{
+            for (int i = left; i >= right; i--) {
+                list.add(i);
+            }
+            return list;
+        }
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
+        List<Integer> list = new ArrayList<>();
+        if(left<right) {
+            for (int i = left; i <= right; i++) {
+                if(i%2==0) {
+                    list.add(i);
+                }
+            }
+            return list;
+        }else{
+            for (int i = left; i >= right; i--) {
+                if(i%2==0) {
+                    list.add(i);
+                }
+            }
+            return list;
+        }
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream( array ).boxed().collect(Collectors.toList()).stream().filter(x->x%2==0).collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return array[array.length-1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
